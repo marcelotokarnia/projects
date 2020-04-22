d925c315538772115d03bec73dfa2935695c73c4
Marcelo Tokarnia <marcelo.tokarnia@klarna.com>
2020-04-22 17:29:30 +0200

    add trekkpedia to the portifolio

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -4,6 +4,10 @@ import djangoNews3 from './django-news3.png'
 import stravaMaps1 from './strava-maps1.png'
 import stravaMaps2 from './strava-maps2.png'
 import stravaMaps3 from './strava-maps3.png'
+import trekkpedia1 from './trekkpedia1.png'
+import trekkpedia2 from './trekkpedia2.png'
+import trekkpedia3 from './trekkpedia3.png'
 
 export const djangoNews = [djangoNews1, djangoNews2, djangoNews3]
 export const stravaMaps = [stravaMaps1, stravaMaps2, stravaMaps3]
+export const trekkpedia = [trekkpedia1, trekkpedia2, trekkpedia3]
--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -1,4 +1,4 @@
-import { stravaMaps, djangoNews } from '../assets/projects'
+import { stravaMaps, djangoNews, trekkpedia } from '../assets/projects'
 
 export default [
   {
@@ -37,16 +37,33 @@ export default [
     link: 'https://marcelotokarnia.github.io',
   },
   {
-    title: 'Svelte Portifolio',
-    text: `It's actually this exact page you are seeing right now 😆.
-    This is a test project to experiment on a new frontend framework (at least for me) called Svelte.
-    It did bring me back nostalgic moments from AngularJS, in a structure similar to Vue (.vue / .svelte files).
-    But with a tooling similar to React. So felt like a big mesh up but I can see it's potential.`,
-    textMore: '',
-    images: stravaMaps,
-    tags: ['Svelte', 'Tachyons', 'Bootstrap', 'Javascript', 'Rollup'],
-    repo: 'https://github.com/marcelotokarnia/marcelotokarnia.github.io',
-    link: 'https://marcelotokarnia.github.io',
+    title: 'Trekkpedia',
+    text: `The main idea behind this project was study about to experiment on Vue Framework.
+    But it grew way bigger than that, I started experimenting on state management with Apollo Link State, 
+    linting with Wotan and even automated blue-green deploys on EC2 instances using docker images generated from the project.
+    It's a simple project, it shows the mountains around you, and tells you how far they are from your position and how high they are.`,
+    textMore: `I learned a lot from this project, how to deal with GDAL and Postgis for georeferenced databases and operations on it.
+    How to store, alter and retrieve Docker instances and how to leverage on those to develop and deploy.
+    I learned a lot about Apollo Link State, which to be honest was not my favorite state manager but still very rewarding to answer a few questions about it on StackOverflow, since not a lot of people were using it back then.
+    Also it was my first Graphene implementation for a Graphql API on a Django Backend, I must say I still prefer Apollo's Node implementation, but any kind of knowledge broadens your potential, it was still worth it.
+    I'm biased to express my opinions about Node anyways 😆`,
+    images: trekkpedia,
+    tags: [
+      'Vue',
+      'Docker',
+      'AWS',
+      'Postgis',
+      'Bootstrap',
+      'Typescript',
+      'Graphql',
+      'Graphene',
+      'Apollo Link State',
+      'Webpack',
+      'Django',
+      'Python',
+    ],
+    repo: 'https://github.com/marcelotokarnia/mountains-catalog',
+    link: 'http://54.90.205.133/',
   },
   {
     title: 'Django News',
